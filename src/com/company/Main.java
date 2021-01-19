089ce1dec45d204a09905622d36b5079d5bc465b
serge160889 <serge_voit_2006@mail.ru>
2021-01-20 02:40:10 +0300

    finish

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.text.DecimalFormat;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,6 +13,28 @@ public class Main {
         dayofBirthday=16;
         int sum;
         sum=yearofBirthday+monthofBirthday+dayofBirthday;
-        System.out.print("Сумма моего года, месяца и дня рождения:"+sum);
+        System.out.println("Сумма моего года, месяца и дня рождения:"+sum);
+
+        boolean mothBiggerDay=(monthofBirthday>dayofBirthday);
+        System.out.println("Месяц моего рождения больше даты рождения:"+mothBiggerDay);
+
+        char[]charArrays=new char[]{'С','Е','Р','Г','Е','Й'};
+        String newString1=new String(charArrays);
+        System.out.println("Мое имя в массиве:"+newString1);
+
+        double myAge;
+        myAge=31;
+        double pathYear;
+                pathYear= (double)5/12;
+        System.out.printf("Мне:");
+        DecimalFormat  format=new DecimalFormat("#.00");
+        System.out.print(format.format(pathYear+myAge));
+        System.out.println("года");
+
+
+        ;
+
+
+
     }
 }
