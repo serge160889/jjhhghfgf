34882612f1fb37806c9fe37faba000c9f1752413
serge160889 <serge_voit_2006@mail.ru>
2021-01-19 22:48:06 +0300

    add 1 part

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int yearfoBirthday;
-
-        System.out.println("hello java");
+        int yearofBirthday;
+        int monthofBirthday;
+        int dayofBirthday;
+        yearofBirthday=1989;
+        monthofBirthday=8;
+        dayofBirthday=16;
+        int sum;
+        sum=yearofBirthday+monthofBirthday+dayofBirthday;
+        System.out.print("Сумма моего года, месяца и дня рождения:"+sum);
     }
 }
